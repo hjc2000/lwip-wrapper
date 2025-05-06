d332c0e7c95277f0b2e93e75cb4bc765190b356e
hjc2000 <hjc.666@foxmail.com>
2025-05-06 15:49:41 +0800

    interrupt

--- private_src/TcpIpInitialize.cpp
+++ private_src/TcpIpInitialize.cpp
@@ -1,6 +1,6 @@
 #include "TcpIpInitialize.h"
-#include <bsp-interface/di/interrupt.h>
-#include <lwip/tcpip.h>
+#include "base/embedded/interrupt/interrupt.h"
+#include "lwip/tcpip.h"
 
 bool volatile _tcp_ip_has_been_initialized = false;
 
@@ -11,7 +11,7 @@ void lwip::TcpIpInitialize()
 		return;
 	}
 
-	bsp::di::interrupt::GlobalInterruptGuard g;
+	base::interrupt::GlobalInterruptionGuard g;
 	_tcp_ip_has_been_initialized = true;
 	tcpip_init(nullptr, nullptr);
 }

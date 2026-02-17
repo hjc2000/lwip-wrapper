c3228c5b1802ea006c72861b18ecec362a361d11
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-02-18 01:20:10 +0800

    ``` feat(TcpIpInitialize): 添加全局中断保护机制
    在TCP/IP初始化过程中引入base::interrupt::GlobalInterruptionGuard，
    确保初始化过程中的中断安全，防止并发访问导致的问题。
    ```

--- private_src/TcpIpInitialize.cpp
+++ private_src/TcpIpInitialize.cpp
@@ -1,4 +1,5 @@
 #include "TcpIpInitialize.h"
+#include "base/embedded/interrupt/interrupt.h"
 #include "lwip/tcpip.h"
 
 bool volatile _tcp_ip_has_been_initialized = false;
@@ -10,6 +11,7 @@ void lwip::TcpIpInitialize()
 		return;
 	}
 
+	base::interrupt::GlobalInterruptionGuard g;
 	_tcp_ip_has_been_initialized = true;
 	tcpip_init(nullptr, nullptr);
 }
